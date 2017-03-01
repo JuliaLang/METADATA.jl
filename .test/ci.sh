3814c3f940be953f79fe63cb6327dd136ba427e9
Curtis Vogt <curtis.vogt@gmail.com>
2017-03-01 14:21:08 -0600

    Made METADATA tests work outside of Travis CI
    - Avoid using Travis specific environmental variables general code
    - Use origin/HEAD over origin/metadata-v2 to allow different default
      branch names

--- .test/ci.sh
+++ .test/ci.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 set -e
 cd $(dirname $0)/..
+BUILD_DIR=$PWD
+[ -z "$CI_TMP_DIR" ] && CI_TMP_DIR=$BUILD_DIR.tmp
+[ -z "$JULIA_PKGDIR" ] && JULIA_PKGDIR=$HOME/.julia
+export BUILD_DIR JULIA_PKGDIR
+
+# Need to be on a local or remote branch for check_metadata
+git checkout -q --detach HEAD && git branch -D localbranch 2>/dev/null || true
 git checkout -b localbranch
-cd ..
-ln -s $PWD/METADATA.jl METADATA
+
+mkdir -p $CI_TMP_DIR
+cd $CI_TMP_DIR
 for ver in 0.4 0.5 0.6; do
-  mkdir -p ~/.julia/v$ver/.cache julia-$ver
-  ln -s $PWD/METADATA.jl ~/.julia/v$ver/METADATA
+  mkdir -p $JULIA_PKGDIR/v$ver/.cache julia-$ver
+  ln -s $BUILD_DIR $JULIA_PKGDIR/v$ver/METADATA
   if [ $ver = 0.6 ]; then
     url="julianightlies/bin/linux/x64/julia-latest-linux64"
   else
     url="julialang/bin/linux/x64/$ver/julia-$ver-latest-linux-x86_64"
   fi
-  curl -A "Travis-CI for METADATA tests $(curl --version | head -n 1)" \
+  curl -A "$CI_NAME for METADATA tests $(curl --version | head -n 1)" \
     -L --retry 5 https://s3.amazonaws.com/$url.tar.gz | \
     tar -C julia-$ver --strip-components=1 -xzf - && \
-    julia-$ver/bin/julia -e 'versioninfo(); include("METADATA/.test/METADATA.jl")' && \
+    julia-$ver/bin/julia -e 'versioninfo(); include("$(ENV["BUILD_DIR"])/.test/METADATA.jl")' && \
     touch success-$ver &
 done
 wait
