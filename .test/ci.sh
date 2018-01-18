88475c21764a3027b0b4e61e397117da424c3599
Avik Sengupta <avik@sengupta.net>
2018-01-18 13:43:07 +0000

    Only run tests on the current release version (#12945)

--- .test/ci.sh
+++ .test/ci.sh
@@ -12,21 +12,21 @@ git checkout -b localbranch
 
 mkdir -p $CI_TMP_DIR
 cd $CI_TMP_DIR
-for ver in 0.4 0.5 0.6 nightly; do
-  if [ $ver = "nightly" ]; then
+
+# Only run on the current release version of Julia
+ver=0.6
+
+if [ $ver = "nightly" ]; then
     url="https://julialangnightlies-s3.julialang.org/bin/linux/x64/julia-latest-linux64.tar.gz"
     ver=0.7
-  else
+else
     url="https://julialang-s3.julialang.org/bin/linux/x64/$ver/julia-$ver-latest-linux-x86_64.tar.gz"
-  fi
-  mkdir -p $JULIA_PKGDIR/v$ver/.cache julia-$ver
-  ln -s $BUILD_DIR $JULIA_PKGDIR/v$ver/METADATA
-  curl -A "$CI_NAME for METADATA tests $(curl --version | head -n 1)" -L --retry 5 $url | \
-    tar -C julia-$ver --strip-components=1 -xzf - && \
-    julia-$ver/bin/julia -e 'versioninfo(); include("$(ENV["BUILD_DIR"])/.test/METADATA.jl")' && \
-    touch success-$ver &
-done
-wait
-if ! [ -e success-0.4 -a -e success-0.5 -a -e success-0.6 ]; then # nightly allowed to fail
-  exit 1
 fi
+mkdir -p $JULIA_PKGDIR/v$ver/.cache julia-$ver
+ln -s $BUILD_DIR $JULIA_PKGDIR/v$ver/METADATA
+curl -A "$CI_NAME for METADATA tests $(curl --version | head -n 1)" -L --retry 5 $url | \
+    tar -C julia-$ver --strip-components=1 -xzf -
+julia-$ver/bin/julia -e 'versioninfo(); include("$(ENV["BUILD_DIR"])/.test/METADATA.jl")'
+touch success-$ver &
+wait
+[ -e success-$ver ] || exit 1
